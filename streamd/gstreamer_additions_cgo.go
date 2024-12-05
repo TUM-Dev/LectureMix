d4111135356498e9d76b7c24c3c1e2e836e60d6d
hmelder <service@hugomelder.com>
2024-12-05 12:40:14 +0100

    streamd: construct net.IP and port from GInetSocketAddress

--- streamd/gstreamer_additions_cgo.go
+++ streamd/gstreamer_additions_cgo.go
@@ -4,9 +4,12 @@ package main
 
 // #cgo pkg-config: glib-2.0 gstreamer-1.0
 // #include <glib-object.h>
+// #include <gio/gio.h>
 // #include <gst/gst.h>
 import "C"
 import (
+	"errors"
+	"net"
 	"unsafe"
 
 	"github.com/go-gst/go-glib/glib"
@@ -39,4 +42,39 @@ func pipelineGetConfiguredLatency(p *gst.Pipeline) gst.ClockTime {
 	ctime := C.gst_pipeline_get_configured_latency((*C.GstPipeline)(ptr))
 
 	return gst.ClockTime(ctime)
+}
+
+const (
+	gSocketFamilyInvalid int = 0
+	gSocketFamilyUnix    int = 1
+	gSocketFamilyIPv4    int = 2
+	gSocketFamilyIPv6    int = 10
+)
+
+// Check that ptr is an GInetSocketAddress
+func inetSocketAddressIP(ptr unsafe.Pointer) (net.IP, uint16, error) {
+	// TODO(hugo) check that ptr is actually a GInetSocketAddress instance
+	sockAddr := (*C.GInetSocketAddress)(ptr)
+
+	port := uint16(C.g_inet_socket_address_get_port(sockAddr))
+
+	inetAddr := C.g_inet_socket_address_get_address(sockAddr)
+	if inetAddr == nil {
+		return nil, 0, errors.New("failed to retrieve address from InetSocketAddress instance")
+	}
+
+	// Transfer: FULL
+	rawAddr := C.g_inet_address_to_string(inetAddr)
+	if rawAddr == nil {
+		return nil, 0, errors.New("failed to convert GInetAddress to string")
+	}
+	defer C.free(unsafe.Pointer(rawAddr))
+
+	addr := C.GoString(rawAddr)
+	ip := net.ParseIP(addr)
+	if ip == nil {
+		return nil, 0, errors.New("failed to parse ip")
+	}
+
+	return ip, port, nil
 }
